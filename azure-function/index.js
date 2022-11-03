951660d0b88ba38cc2aa6f45b989c56a36386a9a
softworkz <softworkz@hotmail.com>
2022-11-03 11:49:12 +0100

    Update

--- azure-function/index.js
+++ azure-function/index.js
@@ -53,7 +53,7 @@ const validateGitHubWebHook = (req) => {
 const triggerAzurePipeline = async (token, organization, project, buildDefinitionId, sourceBranch, parameters) => {
     const auth = Buffer.from('PAT:' + token).toString('base64');
     const headers = {
-        'Accept': 'application/json; api-version=5.0-preview.5; excludeUrls=true',
+        'Accept': 'application/json; api-version=5.0; excludeUrls=true',
         'Authorization': 'Basic ' + auth,
     };
     const json = JSON.stringify({
@@ -67,11 +67,10 @@ const triggerAzurePipeline = async (token, organization, project, buildDefinitio
     const requestOptions = {
         host: 'dev.azure.com',
         port: '443',
-        path: `/${organization}/${project}/_apis/build/builds?ignoreWarnings=false&api-version=5.0-preview.5`,
+        path: `/${organization}/${project}/_apis/build/builds?ignoreWarnings=false&api-version=5.0`,
         method: 'POST',
         headers: headers
     };
-
 
     return new Promise((resolve, reject) => {
         const handleResponse = (res, err) => {
