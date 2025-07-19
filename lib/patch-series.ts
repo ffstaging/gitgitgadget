a1dbcd12a44bb56e860ebfa48ce28769fdace36e
softworkz <softworkz@hotmail.com>
2025-07-19 04:41:57 +0200

    Use same e-mail on cover letter like for patches 2

--- lib/patch-series.ts
+++ lib/patch-series.ts
@@ -474,6 +474,7 @@ export class PatchSeries {
             }
 
             header = authorMatch[1] + replaceSender + authorMatch[3];
+
             ////if (mails.length > 1 && i === 0 && senderName) {
             ////    // skip Cc:ing and From:ing in the cover letter
             ////    mails[i] = header + match[2];
